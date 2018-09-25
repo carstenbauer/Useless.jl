4e67c2959924fd9a8529acb7080d88f2864a42a9
Carsten Bauer <crstnbr@gmail.com>
2018-09-25 14:41:49 +0200

    avoid precompilation

--- src/Useless.jl
+++ src/Useless.jl
@@ -1,5 +1,6 @@
 module Useless
 
-print("This is a completely useless package that you just loaded.")
+println("This is a completely useless package that you just loaded.")
+println("Here is a random number for you: ", rand())
 
 end # module
